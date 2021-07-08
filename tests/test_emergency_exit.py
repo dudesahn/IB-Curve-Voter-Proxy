054b2126c9c5d28d9c52f3d224c76423a9d5fd20
dudesahn <dudesahn@gmail.com>
2021-07-07 21:46:17 -0400

    feat: update tests

--- tests/test_emergency_exit.py
+++ tests/test_emergency_exit.py
@@ -2,57 +2,59 @@ import brownie
 from brownie import Contract
 from brownie import config
 
-def test_emergency_exit(gov, token, vault, dudesahn, strategist, whale, strategyProxy, gaugeIB, rando, chain, amount, StrategyCurveIBVoterProxy, live_strategy, vault_balance, strategist_ms):
-    # Update deposit limit to the vault since it's currently maxed out
-    vault.setDepositLimit(100000000000000000000000000, {"from": strategist_ms})
-    
-    # prepare our live strategy to migrate
-    vault.updateStrategyDebtRatio(live_strategy, 0, {"from": strategist_ms})
-    vault.revokeStrategy(live_strategy.address, {"from": strategist_ms})
-    live_strategy.harvest({"from": dudesahn})
-    
-    # assert that our old strategy is empty
-    assert live_strategy.estimatedTotalAssets() == 0
-
-    # deploy our new strategy
-    strategy = dudesahn.deploy(StrategyCurveIBVoterProxy, vault)
-
-    # migrate our old strategy
-    vault.migrateStrategy(live_strategy, strategy, {"from": strategist_ms})
-
-    # approve on new strategy with proxy
-    strategyProxy.approveStrategy(live_strategy.gauge(), strategy, {"from": gov})
-    vault.updateStrategyDebtRatio(strategy, 10000, {"from": strategist_ms})
+# test passes as of 21-05-20
+def test_emergency_exit(gov, token, vault, dudesahn, strategist, whale, strategy, chain, strategist_ms, strategyProxy, gaugeIB):
+    ## deposit to the vault after approving
+    startingWhale = token.balanceOf(whale)
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(100000e18, {"from": whale})
     strategy.harvest({"from": dudesahn})
-    startingVault = vault.totalAssets()
-    print("\nVault starting assets: ", startingVault)
-    
-    # assert new_strategy.estimatedTotalAssets() >= holdings
-    # assert that our old strategy is empty still
-    assert token.balanceOf(strategy) == 0
-    
+
     # simulate a day of earnings
     chain.sleep(86400)
     chain.mine(1)
-    
-    # Test out our migrated strategy, confirm we're making a profit
-    strategy.harvest({"from": dudesahn})
-    assert strategy.tendCounter() == 0
-    vaultAssets_2 = vault.totalAssets()
-    assert vaultAssets_2 > startingVault
-    print("\nAssets after 1 day harvest: ", vaultAssets_2)
-    
-    ## for default migrations as a part of other tests, just copy all of the text above ##
 
-    # set emergency and exit, then confirm that the strategy has no funds
-    strategy.setEmergencyExit({"from": strategist_ms})
+    # confirm that we will set emergency and exit, then confirm that the strategy has no funds
+    strategy.setEmergencyExit({"from": gov})
     strategy.harvest({"from": dudesahn})
     assert strategy.estimatedTotalAssets() == 0
+    assert strategyProxy.balanceOf(gaugeIB) == 0
 
-    # wait for share price to return to normal
-    chain.sleep(2592000)
+    # simulate a day of waiting for share price to bump back up
+    chain.sleep(86400)
     chain.mine(1)
     
-    # withdraw my money
-    vault.withdraw({"from": dudesahn})    
-    assert token.balanceOf(dudesahn) > 0
+    # withdraw and confirm we made money
+    vault.withdraw({"from": whale})    
+    assert token.balanceOf(whale) > startingWhale 
+
+def test_emergency_shutdown_from_vault(
+    gov, token, vault, whale, strategy, chain, dudesahn, strategyProxy, gaugeIB
+):
+    ## deposit to the vault after approving
+    startingWhale = token.balanceOf(whale)
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(100000e18, {"from": whale})
+    strategy.harvest({"from": dudesahn})
+
+    # simulate a day of earnings
+    chain.sleep(86400)
+    chain.mine(1)
+    strategy.harvest({"from": dudesahn})
+
+    # simulate a day of earnings
+    chain.sleep(86400)
+    chain.mine(1)
+
+    # set emergency and exit, then confirm that the strategy has no funds
+    vault.setEmergencyShutdown(True, {"from": gov})
+    strategy.harvest({"from": gov})
+    assert strategy.estimatedTotalAssets() == 0
+
+    # simulate a day of waiting for share price to bump back up
+    chain.sleep(86400)
+    chain.mine(1)
+
+    # withdraw and confirm we made money
+    vault.withdraw({"from": whale})
+    assert token.balanceOf(whale) >= startingWhale
